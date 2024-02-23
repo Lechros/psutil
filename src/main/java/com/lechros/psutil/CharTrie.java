63037d2f95591637c95d37961e2d1ccdb331dd7b
Lechros <0onionion0@gmail.com>
2024-02-23 17:25:22 +0900

    feat: CharTrie containsSubstring 메소드 추가
    containsSubstring 구현은 아호-코라식 알고리즘을 사용합니다.

--- src/main/java/com/lechros/psutil/CharTrie.java
+++ src/main/java/com/lechros/psutil/CharTrie.java
@@ -1,5 +1,8 @@
 package com.lechros.psutil;
 
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 public class CharTrie {
     public final Node root;
 
@@ -15,11 +18,59 @@ public class CharTrie {
         node.nul = true;
     }
 
+    public void build() {
+        Queue<Node> q = new ArrayDeque<>();
+        root.fail = root;
+        q.offer(root);
+        int size = root.children.length;
+        while (!q.isEmpty()) {
+            Node node = q.poll();
+            for (int i = 0; i < size; i++) {
+                Node child = node.children[i];
+                if (child == null) continue;
+                if (node == root) {
+                    child.fail = root;
+                } else {
+                    Node fail = node.fail;
+                    while (fail != root && fail.children[i] == null) {
+                        fail = fail.fail;
+                    }
+                    if (fail.children[i] != null) {
+                        fail = fail.children[i];
+                    }
+                    child.fail = fail;
+                }
+                if (child.fail.hasNul()) {
+                    child.nul = true;
+                }
+                q.offer(child);
+            }
+        }
+    }
+
+    public boolean containsSubstring(String str) {
+        Node node = root;
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            while (node != root && node.get(ch) == null) {
+                node = node.fail;
+            }
+            if (node.get(ch) != null) {
+                node = node.get(ch);
+            }
+            if (node.hasNul()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     public static class Node {
         private Node[] children;
+        private char offset;
         private boolean nul;
         private int cnt;
-        private char offset;
+        private Node fail;
 
         public Node(char offset, int size) {
             this.offset = offset;
