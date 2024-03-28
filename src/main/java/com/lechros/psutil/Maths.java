8f0118321409b864906d5559bea3e9dfa679ce96
Lechros <0onionion0@gmail.com>
2024-03-28 17:27:58 +0900

    refactor: pow 메소드 Maths로 이동

--- src/main/java/com/lechros/psutil/Maths.java
+++ src/main/java/com/lechros/psutil/Maths.java
@@ -1,6 +1,32 @@
 package com.lechros.psutil;
 
 public class Maths {
+    public static int pow(int a, int n, int m) {
+        long res = 1L;
+        a %= m;
+        while (n > 0) {
+            if ((n & 1) == 1) {
+                res = (res * a) % m;
+            }
+            a = (a * a) % m;
+            n >>= 1;
+        }
+        return (int) res;
+    }
+
+    public static long pow(long a, long n, long m) {
+        long res = 1L;
+        a %= m;
+        while (n > 0) {
+            if ((n & 1) == 1) {
+                res = (res * a) % m;
+            }
+            a = (a * a) % m;
+            n >>= 1;
+        }
+        return res;
+    }
+
     public static int gcd(int a, int b) {
         if (a < b) {
             return gcd(b, a);
