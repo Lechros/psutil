74a5eabf5827ec8a0432b9deec1c370026df7485
Lechros <0onionion0@gmail.com>
2024-04-05 10:42:16 +0900

    refactor: Maths 클래스 분리 및 패키지 이동

--- src/main/java/com/lechros/psutil/math/Gcd.java
+++ src/main/java/com/lechros/psutil/math/Gcd.java
@@ -1,32 +1,6 @@
-package com.lechros.psutil;
+package com.lechros.psutil.math;
 
-public class Maths {
-    public static int pow(int a, int n, int m) {
-        long res = 1L;
-        a %= m;
-        while (n > 0) {
-            if ((n & 1) == 1) {
-                res = (res * a) % m;
-            }
-            a = (a * a) % m;
-            n >>= 1;
-        }
-        return (int) res;
-    }
-
-    public static long pow(long a, long n, long m) {
-        long res = 1L;
-        a %= m;
-        while (n > 0) {
-            if ((n & 1) == 1) {
-                res = (res * a) % m;
-            }
-            a = (a * a) % m;
-            n >>= 1;
-        }
-        return res;
-    }
-
+public class Gcd {
     public static int gcd(int a, int b) {
         if (a < b) {
             return gcd(b, a);
